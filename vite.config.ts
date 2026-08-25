521e21f0c410c7489d570619bad15493eb4131c4
manaiakalani-matt <matt.goodwin@manaiakalani.org>
2026-08-26 07:51:24 +1200

    Target CSS and JavaScript at Safari 12

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  build: { target: ["safari12", "ios12"] },
+  build: { target: ["safari12", "ios12"], cssTarget: "safari12" },
   test: { environment: "node", include: ["src/**/*.test.ts"] },
 });
