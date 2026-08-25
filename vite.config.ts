f72f8be2297e0bf14ffb00aa056e56bbb7b7947b
manaiakalani-matt <matt.goodwin@manaiakalani.org>
2026-08-25 16:26:53 +1200

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+  build: { target: ["safari12", "ios12"] },
+  test: { environment: "node", include: ["src/**/*.test.ts"] },
+});
